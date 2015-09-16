c0668c28c3a451fea95687b9c90c590e9d2cd189
Antonio S <asanchez@hispasec.com>
2015-09-16 07:25:29 +0200

    Prepared for pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='Koodous',
+setup(name='koodous-py',
+      packages=['koodous'],
       version='0.1',
       description='Module to interact with Koodous API',
       author='Antonio Sanchez',
       author_email='asanchez@koodous.com',
       license='Apache Version 2',
       url='https://koodous.com/',
-      packages=['koodous'],
+      download_url='https://github.com/Koodous/python-sdk/archive/master.zip',
+      keywords=['koodous', 'api', 'sdk', 'python', 'android', 'apk', 'malware']
      )
