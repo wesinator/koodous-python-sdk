ba56725a1df3047b238f8103e77c2f896fc50a33
Antonio S <asanchez@hispasec.com>
2016-01-19 09:45:46 +0100

    Modified setup to fix problem with requirements

--- setup.py
+++ setup.py
@@ -1,33 +1,26 @@
 #!/usr/bin/env python
 import pip
-from pip.req import parse_requirements
 from setuptools import setup
 
-REQUIREMENTS_FILE = 'requirements.txt'
-
-try:
-    install_reqs = parse_requirements(REQUIREMENTS_FILE,
-                                      session=pip.download.PipSession())
-except AttributeError as ex:
-    raise ex
-
-    import warnings
-    warnings.warn('Your PIP is very old: we strongly recommend to update it')
-
-reqs = [str(ir.req) for ir in install_reqs]
+REQUIREMENTS_FILE = 'requires.txt'
 
 setup(name='koodous-py',
       packages=['koodous'],
-      version='0.3',
+      version='0.7',
       description='Module to interact with Koodous API',
       author='Antonio Sanchez',
       author_email='asanchez@koodous.com',
       license='Apache Version 2',
       url='https://koodous.com/',
       download_url='https://github.com/Koodous/python-sdk/archive/master.zip',
-      keywords=['koodous', 'api', 'sdk', 'python', 'android', 'apk',
-                'malware'],
-      install_requires=reqs,
+      keywords=['koodous', 'api', 'sdk', 'python', 'android', 'apk', 'malware'],
+      install_requires=["click==6.2", 
+                        "coloredlogs==5.0", 
+                        "humanfriendly==1.42", 
+                        "Pygments==2.0.2", 
+                        "requests==2.8.1", 
+                        "androguard==3.0", 
+                        "certifi"],
       entry_points='''
         [console_scripts]
         koocli=koodous.cli:cli
